6735f97d51713e62d92fd64abc3ac65c078d6066
Matt <mwaterman29@gmail.com>
2024-10-23 22:07:25 -0400

    group cornerstones

--- app/cornerstones/page.tsx
+++ app/cornerstones/page.tsx
@@ -1,3 +1,4 @@
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible';
 import * as EffectsData from '@/data/effects.json';
 import { cn } from '@/lib/utils';
 import { Cornerstone } from '@/types/Cornerstone';
@@ -6,61 +7,102 @@ import { Cornerstone } from '@/types/Cornerstone';
 type si = { [key: string]: string };
 
 const borderMapping: si = {
-    'Mythic': 'border-perk-stormforged',
-    'Legendary': 'border-perk-orange',
-    'Epic': 'border-perk-purple',
-    'Rare': 'border-perk-blue',
-    'Uncommon': 'border-perk-green',
-}
+	Mythic: 'border-perk-stormforged',
+	Legendary: 'border-perk-orange',
+	Epic: 'border-perk-purple',
+	Rare: 'border-perk-blue',
+	Uncommon: 'border-perk-green'
+};
 
 const innerBorderMapping: si = {
-    'Mythic': 'border-color-gold',
-    'Legendary': 'border-perk-orange',
-    'Epic': 'border-perk-purple',
-    'Rare': 'border-perk-blue',
-    'Uncommon': 'border-perk-green',
-}
+	Mythic: 'border-color-gold',
+	Legendary: 'border-perk-orange',
+	Epic: 'border-perk-purple',
+	Rare: 'border-perk-blue',
+	Uncommon: 'border-perk-green'
+};
 
 const textMapping: si = {
-    'Mythic': 'text-perk-stormforged',
-    'Legendary': 'text-perk-orange',
-    'Epic': 'text-perk-purple',
-    'Rare': 'text-perk-blue',
-    'Uncommon': 'text-perk-green',
-}
+	Mythic: 'text-perk-stormforged',
+	Legendary: 'text-perk-orange',
+	Epic: 'text-perk-purple',
+	Rare: 'text-perk-blue',
+	Uncommon: 'text-perk-green'
+};
 
-const CornerstoneComponent = ({ cornerstone }: { cornerstone: Cornerstone }) => 
+const CornerstoneComponent = ({ cornerstone }: { cornerstone: Cornerstone }) =>
 {
-    return (
-        <div className={cn('border rounded-md p-4 flex flex-row gap-4', borderMapping[cornerstone.tier])}>
-            <div className={cn('h-16 aspect-square border ', innerBorderMapping[cornerstone.tier])}>
-                <img
-                
-                />
-            </div>
+	return (
+		<div className={cn('border rounded-md p-4 flex flex-row gap-4', borderMapping[cornerstone.tier])}>
+			<div className={cn('h-16 aspect-square border ', innerBorderMapping[cornerstone.tier])}>
+				<img />
+			</div>
 
-            <div className='flex flex-col gap-2'>
-                <p className={cn('text-lg font-bold', textMapping[cornerstone.tier])}>{cornerstone.label}</p>
-                <p className='text-slate-300'>{cornerstone.description}</p>
-            </div>
-            
-        </div>
-    )
-}
+			<div className='flex flex-col gap-2'>
+				<p className={cn('text-lg font-bold', textMapping[cornerstone.tier])}>{cornerstone.label}</p>
+				<p className=''>{cornerstone.description}</p>
+			</div>
+		</div>
+	);
+};
 
-const CornerstonesPage = () => {
-    
-    const cornerstones: Cornerstone[] = EffectsData.filter(effect => effect.type === 'Cornerstone');
-    const stormforged = cornerstones.filter(cornerstone => cornerstone.tier === 'Mythic');
-    const annual = cornerstones.filter(cornerstone => cornerstone.tier === 'Legendary' || cornerstone.tier === 'Epic');
+const CornerstonesPage = () =>
+{
+	const cornerstones: Cornerstone[] = EffectsData.filter(effect => effect.type === 'Cornerstone');
+	const stormforged = cornerstones.filter(cornerstone => cornerstone.tier === 'Mythic');
+	const annual = cornerstones.filter(cornerstone => cornerstone.tier === 'Legendary' || cornerstone.tier === 'Epic');
+	const legendary = cornerstones.filter(cornerstone => cornerstone.tier === 'Legendary');
+	const epic = cornerstones.filter(cornerstone => cornerstone.tier === 'Epic');
 
-    return (
-        <div className='flex flex-col gap-2 p-2 overflow-y-auto max-h-full bg-slate-900'>
-            {cornerstones.map((cornerstone, index) => {
-                return <CornerstoneComponent key={index} cornerstone={cornerstone} />
-            })}
-        </div>
-    )
-}
+
+	return (
+		<div className='flex flex-col gap-2 p-2 overflow-y-auto max-h-full bg-slate-900 text-slate-300'>
+			<Collapsible>
+				<CollapsibleTrigger>
+					<p className='text-xl text-white'>
+						<span className='text-perk-stormforged font-bold'>Stormforged</span> Cornerstones
+					</p>
+				</CollapsibleTrigger>
+				<CollapsibleContent className='flex flex-col gap-2'>
+					{stormforged.map((cornerstone, index) =>
+					{
+						return <CornerstoneComponent key={index} cornerstone={cornerstone} />;
+					})}
+				</CollapsibleContent>
+			</Collapsible>
+
+			<Collapsible>
+				<CollapsibleTrigger>
+					<p className='text-xl text-white'>
+						<span className='text-perk-orange font-bold'>Annual</span> Cornerstones
+					</p>
+				</CollapsibleTrigger>
+				<CollapsibleContent className='flex flex-col gap-2'>
+					<div className='flex flex-col py-2'>
+						<p className='text-white text-lg'><span className='text-perk-orange'>Legendary</span> Cornerstones</p>
+						<p className=''><span className='text-perk-orange'>Legendary</span> Cornerstones are offered at the start of <strong>even years</strong>.</p>
+					<p></p>
+					<hr/>
+					</div>
+					{legendary.map((cornerstone, index) =>
+					{
+						return <CornerstoneComponent key={index} cornerstone={cornerstone} />;
+					})}
+					
+					<div className='flex flex-col py-2'>
+						<p className='text-white text-lg'><span className='text-perk-purple'>Epic</span> Cornerstones</p>
+						<p className=''><span className='text-perk-purple'>Epic</span> Cornerstones are offered at the start of <strong>even years</strong>.</p>
+					<p></p>
+					<hr/>
+					</div>
+					{epic.map((cornerstone, index) =>
+					{
+						return <CornerstoneComponent key={index} cornerstone={cornerstone} />;
+					})}
+				</CollapsibleContent>
+			</Collapsible>
+		</div>
+	);
+};
 
 export default CornerstonesPage;
